076873e15642c23f0fa510505182bdc58eb23322
Anna Senchukova <a.d.senchukova@gmail.com>
2026-08-06 09:41:15 +0300

    feat/js: dev: добавлен скрипт аккордеона из другого проекта

--- js/script.js
+++ js/script.js
@@ -74,3 +74,59 @@ document.querySelectorAll('dialog.modal').forEach((dialog) => {
         document.body.classList.remove('modal-open');
     });
 });
+
+
+
+/* script accordion */
+
+function ready() {
+    var answer = document.querySelectorAll('.accordion__answer');
+    var questionButtons = document.querySelectorAll('.accordion__question');
+
+    function buttonArrowClose(item) {
+        item.classList.remove('accordion__question--arrow-up');
+        item.classList.add('accordion__question--arrow-down');
+    }
+
+    function buttonArrowOpen(item) {
+        item.classList.remove('accordion__question--arrow-down');
+        item.classList.add('accordion__question--arrow-up');
+    }
+
+    function answerClose(currentContent) {
+        currentContent.classList.remove('accordion__answer--nojs');
+        currentContent.classList.remove('accordion__answer--open');
+        currentContent.classList.add('accordion__answer--close');
+    }
+
+    function answerOpen(currentContent) {
+        questionButtons.forEach((item) => {
+            buttonArrowClose(item);
+        });
+
+        answer.forEach((item) => {
+            answerClose(item);
+        });
+        currentContent.classList.remove('accordion__answer--close');
+        currentContent.classList.add('accordion__answer--open');
+    }
+
+    answer.forEach((item, i) => {
+        answerClose(item);
+    });
+
+    questionButtons.forEach(function(item, i) {
+        buttonArrowClose(item);
+
+        item.addEventListener('click', function(e) {
+            if (answer[i].classList.contains('accordion__answer--close')) {
+                answerOpen(answer[i]);
+                buttonArrowOpen(item);
+            }
+            else {
+                answerClose(answer[i]);
+                buttonArrowClose(item);
+            }
+        });
+    });
+}
